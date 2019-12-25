d75525cfff0216c202741d921344605f7271fb1b
Bill Hunter <billhuntergo@gmail.com>
2019-12-25 17:46:45 +0800

    ok

--- math.go
+++ math.go
@@ -5,25 +5,40 @@ import (
 )
 
 // a + b
-func Add(a, b float64) float64 {
-	f, _ := decimal.NewFromFloat(a).Add(decimal.NewFromFloat(b)).Round(2).Float64()
+func Add(a float64, b ...float64) float64 {
+	m := decimal.NewFromFloat(a)
+	for _, v := range b {
+		m = m.Add(decimal.NewFromFloat(v))
+	}
+	f, _ := m.Round(2).Float64()
+	//f, _ := decimal.NewFromFloat(a).Add(decimal.NewFromFloat(b)).Round(2).Float64()
 	return f
 }
 
 // a - b
-func Sub(a, b float64) float64 {
-	f, _ := decimal.NewFromFloat(a).Sub(decimal.NewFromFloat(b)).Round(2).Float64()
+func Sub(a float64, b ...float64) float64 {
+	m := decimal.NewFromFloat(a)
+	for _, v := range b {
+		m = m.Sub(decimal.NewFromFloat(v))
+	}
+	f, _ := m.Round(2).Float64()
+	//f, _ := decimal.NewFromFloat(a).Sub(decimal.NewFromFloat(b)).Round(2).Float64()
 	return f
 }
 
 // a * b
-func Mul(a, b float64) float64 {
-	f, _ := decimal.NewFromFloat(a).Mul(decimal.NewFromFloat(b)).Round(2).Float64()
+func Mul(a float64, b ...float64) float64 {
+	m := decimal.NewFromFloat(a)
+	for _, v := range b {
+		m = m.Mul(decimal.NewFromFloat(v))
+	}
+	f, _ := m.Round(2).Float64()
+	//f, _ := decimal.NewFromFloat(a).Mul(decimal.NewFromFloat(b)).Round(2).Float64()
 	return f
 }
 
 // a / b
-func Div(a, b float64) float64 {
+func Div(a float64, b float64) float64 {
 	f, _ := decimal.NewFromFloat(a).Div(decimal.NewFromFloat(b)).Round(2).Float64()
 	return f
 }
